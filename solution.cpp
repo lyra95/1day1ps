0bc782325b0a1148afb2ed7e71e012b05bc3dc1a
jo_laptop <95hyouka@gmail.com>
2021-04-13 17:00:06 +0900

    20210418

--- solution.cpp
+++ solution.cpp
@@ -6,18 +6,62 @@
 
 #include <vector>
 #include <iostream>
+#include <queue>
+#include <algorithm>
 using namespace std;
 // solution
 typedef int answerType;
-
+class Printer
+{
+    public:
+        int location;
+        queue<int> q;
+        Printer(vector<int> priorities, int location)
+        :location(location)
+        {
+            queue<int> q;
+            for(int x :priorities)
+            {
+                q.push(x);
+            }
+            this->q = q;
+        }
+        void print()
+        {
+            q.pop();
+            location--;
+        }
+        void delay()
+        {
+            int first = q.front();
+            q.pop();
+            q.push(first);
+            (location == 0) ? location=q.size()-1 : location--;
+        }
+        int front()
+        {
+            return q.front();
+        }
+};
 answerType solution( vector<int> priorities, int location )
 {
-    int i = 0;
-    while(true)
+    Printer p(priorities, location);
+    sort(priorities.begin(),priorities.end());
+    reverse(priorities.begin(),priorities.end());
+    int cursor=0;
+    while(p.location >= 0)
     {
-        
+        if(p.front() >= priorities[cursor])
+        {
+            p.print();
+            cursor++;
+        }
+        else
+        {
+            p.delay();
+        }
     }
-    return 0;
+    return cursor;
 }
 
 // test
@@ -42,6 +86,7 @@ class Test
     
     void test()
     {   
+        helper();
         std::cout << "total " << test_cases.size() << " cases" << std::endl;
         int count = 0;
         for(int i=0;i<test_cases.size();i++)
@@ -66,5 +111,22 @@ class Test
             }
         }
         std::cout << "passed total " << count << " cases over " << test_cases.size() << " cases." << std::endl;
+    }
+    void helper()
+    {
+        {
+            vector<int> priorities{2,1,3,2};
+            int location=2;
+            int answer=1;
+            TestInput t{priorities,location};
+            push(t,answer);
+        }
+        {
+            vector<int> priorities{1,1,9,1,1,1};
+            int location=0;
+            int answer=5;
+            TestInput t{priorities,location};
+            push(t,answer);
+        }
     }
 };
